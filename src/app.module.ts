c4c689a568868aa4a8812148814ebbd84c38c589
hrvoje <hrvoje.cukman@gmail.com>
2025-04-24 09:38:04 +0200

    Update app.module.ts: Add ConfigModule for global configuration, include PrismaModule and StripeModule in imports, and remove redundant TwilioModule import to enhance module organization.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { AdminModule } from './admin/admin.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { TwilioModule } from './twilio/twilio.module';
+import { UserModule } from './user/user.module';
+import { StripeModule } from './stripe/stripe.module';
+import { AdminModule } from './admin/admin.module';
 import { VerificationModule } from './verification/verification.module';
 
 @Module({
-  imports: [AuthModule, UserModule, AdminModule, TwilioModule, VerificationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    PrismaModule,
+    TwilioModule,
+    UserModule,
+    StripeModule,
+    AdminModule,
+    VerificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
