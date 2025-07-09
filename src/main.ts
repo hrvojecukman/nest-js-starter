f08f1f4dd06e939badcdf44d9060affd606520f0
hrvoje <hrvoje.cukman@gmail.com>
2025-07-09 20:28:20 +0200

    Enable CORS in main application setup: Configure CORS settings to allow requests from specified origins and define allowed methods and headers for enhanced security and flexibility in API interactions.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,24 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',     
+      // 'https://yourdomain.com',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+      'X-API-Key',
+    ],
+    credentials: true, 
+  });
+
   app.setGlobalPrefix('api/v1');
 
   await app.listen(3000);
